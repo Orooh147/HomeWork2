708796d04d03668b6a238036c5ad9d30f6d7aa56
Luddifix <Orooh147@gmail.com>
2020-12-02 23:20:34 +0200

    Added Homework

---
+++ TESTFLOAT/Assets/Scripts/MyPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayer : MonoBehaviour
+{
+    float pHealth;
+
+    void Start()
+    {
+        pHealth = SingleTone.Instance.Health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Tookdmg(pHealth);
+        }
+        Debug.Log(pHealth);
+    }
+
+    public float Tookdmg(float damage)
+    {
+        damage = 15;
+        pHealth -= damage;
+        if (pHealth <=0)
+        {
+            Die();
+            Debug.Log("Died");
+        }
+
+        return damage;
+            //Tookdmg(pHealth);
+    }
+
+    void Die()
+    {
+        Instantiate(this.gameObject);
+        Instantiate(this.gameObject);
+        Destroy(this.gameObject);
+    }
+}
---
+++ TESTFLOAT/Assets/Scripts/Rbuff.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rbuff : MonoBehaviour
+{
+    void Update()
+    {
+      //Check which   
+    }
+    public void RedBuff()
+    {
+        //Here is the actual actions
+    }
+
+}
